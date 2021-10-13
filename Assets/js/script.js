7c91839a16f88d69e6d260e5e5d15bdbad7ad96e
Montii <llh9@yahoo.com>
2021-10-13 04:17:06 -0400

    clean like Mr.Clean with README update.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 // When the site loads
-//references to buttons
+//references to buttons and text areas
 var startBtn = document.querySelector("#startBtn");
 var timer = document.querySelector("#timer");
 var textArea = document.querySelector("#question");
@@ -14,111 +14,160 @@ var cBtn = document.querySelector("#cBtn");
 var dBtn = document.querySelector("#dBtn");
 var prev = document.querySelector("#Previous");
 var next = document.querySelector("#Next");
-//logic arrays
+
+//Ordered Arrays
 var qstnArray = ["What's my favorite color?","What is the name of the main character in the movie 'Nightmare Before Christmas?","Which weighs more? ","How much wood would a wood pecker peck if wood pecker could peck wood?","How tall was Abraham Lincoln"];
 var numArray = ["#1","#2","#3","#4","#5"];
-var rtAnsArray = ["d","c","d","d","a"];
-var chAnsArray = ['','','','',''];
 var aArray = ["blue","Cinderella","a lb of fish","none","6ft 4in"];
 var bArray = ["white","Jack Black","a lb of rocks","all","6ft 1in"];
 var cArray = ["purple","Jack Skelington","a lb of air","most","6ft 6in"];
 var dArray = ["red & black","Landon","none of the above","as much as is needed to get them bugs out of there","6ft"];
+
+//Compared Arrays
+var rtAnsArray = ["d","c","d","d","a"];
+var chAnsArray = ['','','','',''];
+
+//Initial Timer Setting
 var secondsLeft = 120;
+
 //when the start button is clicked
 startBtn.addEventListener("click", function(event){
     event.preventDefault();
 
-    //Ask user for a user name. Store respinse in "var user;"
+    //Ask user for a user name. Store response in "var user;"(active key)
     var user = prompt("Please enter your User Name");
 
     //If user is retaking the quiz, access thier user file.
     var userSearcher = localStorage.getItem(user);
 
-    //If user is taking the quiz for the first time then make a new user file who's key is their given User Name 
+    //If user is taking the quiz for the first time...then make a new user file who's key is their given User Name 
     if(userSearcher == null){
+
+        //Create temperary objet to declare a new key and set of values 
         var initUser = {
+
             timesTaken: "0", 
             highScore: "0", 
             currentScore: "0"
         };
 
-        //Setup New User File
+        //Copy temperary object as a string and store that string(active user stats) in local storage
         localStorage.setItem(user, JSON.stringify(initUser));
 
     }
 
+    //Set timer
     var timerInterval
     function setTime() {
-        // Sets interval in variable
-            timerInterval = setInterval(function() {
-          secondsLeft--;
-          timer.textContent = secondsLeft + "s";
+
+        // Sets interval of time between decrementing and re-displaying of timer text
+        timerInterval = setInterval(function() {
+
+            //Decrement timer value
+            secondsLeft--;
+            //Display timer value 
+            timer.textContent = secondsLeft + "s";
       
-          if(secondsLeft === 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append image
-            alert("Your time has run out!");
-            getResults();
-          }
-      
+            //If time is up...
+            if(secondsLeft === 0) { 
+                // ...Stops execution of action at set interval...
+                clearInterval(timerInterval);
+                // ...Notify the user that the timer has reached 0s... 
+                alert("Your time has run out!");
+                // ...End quiz, Display after I
+                getResults();//;)
+            } 
+
+        //1 second 
         }, 1000);
+
     }
+
+    //Start timer
     setTime();
 
     //Place first question and set of answers into txt areas
     var crntQ = 0;
     console.log(crntQ);
     if(crntQ == 0){
+
+        //Display next question 
         textArea.textContent = qstnArray[crntQ];
+        //Display next question number
         qNum.textContent = numArray[crntQ];
+        //Display next answer of A, B, C, and D
         aTxt.textContent = aArray[crntQ];
         bTxt.textContent = bArray[crntQ];
         cTxt.textContent = cArray[crntQ];
         dTxt.textContent = dArray[crntQ];
+        //Increment  current question 
         crntQ = crntQ + 1;
+
     }
 
     //when an answer button is clicked. 
     aBtn.addEventListener("click", function(event){
+
+        //Store the letter of this Button in the coresponding chosen answer array index
         chAnsArray[crntQ - 1] = 'a';
+
         //if answering last question, calculate & display results
         if(crntQ == 5){
+
             getResults();
+
         }else{
         nxt();
         }
+
     });
 
     bBtn.addEventListener("click", function(event){
+
+        //Store the letter of this Button in the coresponding chosen answer array index
         chAnsArray[crntQ - 1] = 'b';
+
         //if answering last question, calculate & display results
         if(crntQ == 5){
+
             getResults();
+
         }else{
         nxt();
         }
+
     });   
 
     cBtn.addEventListener("click", function(event){
+
+        //Store the letter of this Button in the coresponding chosen answer array index
         chAnsArray[crntQ - 1] = 'c';
+
         //if answering last question, calculate & display results
         if(crntQ == 5){
+
             getResults();
+
         }else{
         nxt();
         }
+
     });    
 
     dBtn.addEventListener("click", function(event){
+
+        //Store the letter of this Button in the coresponding chosen answer array index
         chAnsArray[crntQ - 1] = 'd';
+
         //if answering last question, calculate & display results
         if(crntQ == 5){
+
             getResults();
-            return;
+            
         }else{
         nxt();
         }
+
     });
 
     console.log(crntQ);
@@ -138,12 +187,17 @@ startBtn.addEventListener("click", function(event){
 
     //Calculate and Display Results
     function getResults(){
+
+        //Disable Buttons
         document.getElementById("aBtn").disabled = true;
         document.getElementById("bBtn").disabled = true;
         document.getElementById("cBtn").disabled = true;
         document.getElementById("dBtn").disabled = true;
         document.getElementById("Previous").disabled = true;document.getElementById("Next").disabled = true;
+
+        //Stop Timer
         clearInterval(timerInterval);
+
         //extract local storage values and update them 
         //extract entire object
         var userUpdate = JSON.parse(localStorage.getItem(user));
@@ -156,26 +210,37 @@ startBtn.addEventListener("click", function(event){
         var hScore = parseInt(userUpdate.highScore);
         console.log(hScore);
         var score = 0;
+
         //calculate score
         for(i = 0; i < 5; i++){
+
+            //For each index, if the two Arrays are equal, then add 1 to the score
             if(chAnsArray[i] == rtAnsArray[i] ){
+
                 score++;
             };
+
         };
         console.log(chAnsArray);
         console.log(rtAnsArray);
+
         //compare previous high score to current score. Update if score is the new high score 
         if(score > hScore){
+
             hScore = score;
         }
         console.log(score);
         console.log(hScore);
+
         //Restore user stats
         var updatedUser = {
+            
+            //Create temperary object to locate active stats
             timesTaken: JSON.stringify(timesTook), 
             highScore: JSON.stringify(hScore), 
             currentScore: JSON.stringify(score)
         };  
+        //Copy temperary object as a string and store that string(active user stats) in local storage
         localStorage.setItem(user, JSON.stringify(updatedUser));
         console.log(localStorage.getItem(user));
 
@@ -184,31 +249,39 @@ startBtn.addEventListener("click", function(event){
             
     }
 
-   
+    //Navigate to the next question
     function nxt(){
+        
+        //Display next question 
         textArea.textContent = qstnArray[crntQ];
+        //Display next question number
         qNum.textContent = numArray[crntQ];
+        //Display next answer of A, B, C, and D
         aTxt.textContent = aArray[crntQ];
         bTxt.textContent = bArray[crntQ];
         cTxt.textContent = cArray[crntQ];
         dTxt.textContent = dArray[crntQ];
-
+        //Increment  current question
         crntQ = crntQ + 1;
 
     }
 
     function last(){
+
+        //Decrement current question
         crntQ = crntQ - 1;
+        //Display next question 
         textArea.textContent = qstnArray[crntQ];
+        //Display next question number
         qNum.textContent = numArray[crntQ];
+        //Display next answer of A, B, C, and D
         aTxt.textContent = aArray[crntQ];
         bTxt.textContent = bArray[crntQ];
         cTxt.textContent = cArray[crntQ];
         dTxt.textContent = dArray[crntQ];
 
     }
-    //when nav button is clicked 
-    //when highlighted submit button is cliked 
+    
   
 });
 
--- README.md
+++ README.md
@@ -1,89 +1,42 @@
-# 04 Web APIs: Code Quiz
+# Random Quiz
 
-## Your Task
+## Task
 
-At some point in your journey to become a full-stack web developer, you’ll likely be asked to complete a coding assessment&mdash;perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. 
+I was given the task of building a timed coding quiz with multiple-choice questions. This app runs in the browser and feature's dynamically updated HTML and CSS powered by JavaScript code that I wrote. It has a clean, polished, and responsive user interface. 
 
-To help familiarize you with these tests and allow you to use the skills covered in this unit, this week’s homework invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
-
-This week’s coursework will equip you with all the skills you need to succeed in this assignment.
+This week’s coursework activities have helped me to succeed in this assignment.
 
 ## User Story
 
 ```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
+AS A Dad
+I WANT to take a timed quiz on random facts a child might ask that also stores high scores
+SO THAT I can gauge my progress compared to my peers.
 ```
 
 ## Acceptance Criteria
 
 ```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
+Given this is a Random Quiz, when the start button is clicked the user is asked for a username that is used to keep track of stats. Once a response is given, the timer starts and the first question and set of answers  are dispayed. Whem a question is answered, another question is presented. When a question is answered incorrectly the timer continues to decrement.When all questions are answered or the timer reaches 0, the game is over and the user stats are stored and displayed.
 ```
 
-## Mock-Up
+## Screen-Shot
 
-The following animation demonstrates the application functionality:
+The following Image is a screenshot of my ap:
 
-![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/images/04-web-apis-homework-demo.gif)
+![A screen shot of the Random Quiz showing the final display and the localy stored object](./Assets/images/quiz-screenshot.PNG)
 
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
 
 ## Review
 
-You are required to submit BOTH of the following for review:
+I tried to perform more encapsulization within the javaScript code for this application compared to my previous developments, and did so from scratch. 
+With the exception of the timer functions, all of the JS code is written from my original ideas, refering to activities and the internet for syntax only.
+I modified the HTML and CSS from the Password Generator to create a GUI for this multiple choice quiz.
 
-* The URL of the functional, deployed application.
+### Link to Deployment
+https://llh9.github.io/Random-Quiz/
 
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
+### Developer - Landon Hinkle
 ---
 
 © 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
